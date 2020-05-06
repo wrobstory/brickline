b89ff7b18cde86a81bcd897aebf539dea271e7a3
Rob Story <wrobstory@gmail.com>
2020-05-06 14:43:14 -0700

    Add fold to build item map

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 pub mod inventory;
 
-use crate::inventory::{Color, ItemID};
+use crate::inventory::{Inventory, Item, Color, ItemID};
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Error as IOError, Read};
 use std::path::PathBuf;
@@ -13,8 +14,16 @@ pub fn xml_to_string(file_path: &PathBuf) -> Result<String, IOError> {
     Ok(xml_string)
 }
 
+fn build_item_color_hash_table(inventory: &Inventory) -> HashMap<ItemColorHashKey, &Item> {
+    inventory.items.iter().fold(HashMap::new(), |mut acc, item| {
+        let item_color_key = ItemColorHashKey { item_id: &item.item_id, color: &item.color };
+        acc.insert(item_color_key, item);
+        acc
+    })
+}
+
 #[derive(Debug, PartialEq, Eq, Hash)]
-struct ItemColorHashKey {
-    item_id: ItemID,
-    color: Color,
+struct ItemColorHashKey<'a> {
+    item_id: &'a ItemID,
+    color: &'a Option<Color>,
 }
